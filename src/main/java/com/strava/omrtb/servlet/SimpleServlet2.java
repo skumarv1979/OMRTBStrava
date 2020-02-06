7d9ef147368ac66b3bb402db22ecf5676d762506
Saravanakumar Venkatachalam <skumarv@hotmail.com>
2020-02-06 14:24:57 +0530

    Strava auth

--- src/main/java/com/strava/omrtb/servlet/SimpleServlet2.java
+++ src/main/java/com/strava/omrtb/servlet/SimpleServlet2.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Properties;
 
@@ -15,27 +16,30 @@ import javax.servlet.http.HttpServletResponse;
 import javastrava.api.API;
 import javastrava.api.AuthorisationAPI;
 import javastrava.auth.AuthorisationService;
+import javastrava.auth.TokenManager;
 import javastrava.auth.impl.AuthorisationServiceImpl;
 import javastrava.auth.model.Token;
 import javastrava.auth.model.TokenResponse;
 import javastrava.model.StravaActivity;
 import javastrava.model.StravaAthlete;
+import javastrava.model.StravaClub;
 import javastrava.service.ActivityService;
-import javastrava.service.AthleteService;
-import javastrava.service.WebhookService;
+import javastrava.service.ClubService;
+import javastrava.service.Strava;
 import javastrava.service.impl.ActivityServiceImpl;
+import javastrava.service.impl.ClubServiceImpl;
 import javastrava.util.Paging;
 
 /**
  * Servlet implementation class SimpleServlet
  */
-public class SimpleServlet extends HttpServlet {
+public class SimpleServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SimpleServlet() {
+    public SimpleServlet2() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -45,32 +49,16 @@ public class SimpleServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Do Get method");
 		resp.setContentType("text/plain");
-        /*Enumeration<String> parameterNames = req.getParameterNames();
-        
-        while (parameterNames.hasMoreElements()) {
- 
-            String paramName = parameterNames.nextElement();
-            System.out.println(paramName);
- 
-            String[] paramValues = req.getParameterValues(paramName);
-            for (int i = 0; i < paramValues.length; i++) {
-                String paramValue = paramValues[i];
-                System.out.println("\t" + paramValue);
-            }
- 
-        }*/
- 
 
-		//resp.getWriter().append("Served at: ").append(req.getContextPath());
+		resp.getWriter().append("Served at: ").append(req.getContextPath());
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		//InputStream input = getServletContext().getResourceAsStream("/application.properties");
 		InputStream input = classLoader.getResourceAsStream("application.properties");
 		
 		Properties prop = new Properties();
 		prop.load(input);
-		//System.out.println("strava.client_id :: "+ prop.get("strava.client_id"));
+		System.out.println("strava.client_id :: "+ prop.get("strava.client_id"));
 		int clientId = Integer.parseInt((String) prop.get("strava.client_id"));
 		String clientSecret = prop.get("strava.client_secret").toString();
 		System.out.println("clientId :: "+clientId);
@@ -78,41 +66,32 @@ public class SimpleServlet extends HttpServlet {
 		//TokenManager tokenManager= TokenManager.instance();
 		//tokenManager.retrieveTokenWithScope("skumarvdlf@gmail.com");
 		//if(tokenManager==null || tokenManager.ge)
-		//AuthorisationService service = new AuthorisationServiceImpl();
 		String code = req.getParameter("code");
-		//System.out.println("Code :: " + code);
+		System.out.println("Code :: " + code);
 		//Token token = service.tokenExchange(clientId, clientSecret, code);
 		
-		MyStravaAuthorisationAPI myAuth = MyStravaAPI.authorisationInstanceMy();
-		MyStravaTokenResponse response = null;
-		try {
-			response = myAuth.tokenExchangeMyStrava(clientId, clientSecret, code, "authorization_code");
-		}
-		catch (Exception e) {
-			MyStravaRefreshTokenAPI refersh = MyStravaAPI.authorisationInstanceMyRefresh();
-			response = refersh.tokenExchangeMyStravaRefresh(clientId, clientSecret, "refresh_token", "862b16f24f8f3300caacbc6d2f1c5918adf3f820");
-		}
-		
-		//AuthorisationAPI auth = API.authorisationInstance();
-		//TokenResponse response = auth.tokenExchange(clientId, clientSecret, code, "authorization_code");
-		System.out.println("respn :: "+response.getTokenType()+", "+response.getRefreshToken()+", "+response.getAccessToken());
-		response.getTokenType();
+		AuthorisationAPI auth = API.authorisationInstance();
+		TokenResponse response = auth.tokenExchange(clientId, clientSecret, code);
 		Token token = new Token(response);
-		AthleteService asss = token.getService(AthleteService.class);
-		ActivityService sa = ActivityServiceImpl.instance(token);
-		StravaAthlete straAth = asss.getAuthenticatedAthlete();
-		System.out.println("Atyhlete ID : "+straAth.getId());
-		StravaAthlete toAth = token.getAthlete();
-		System.out.println("token Athlete :: "+toAth.getId()+", "+token.getToken()+", ");
-		//Token token = service.tokenExchange(clientId, clientSecret, code);
-		Paging page = new Paging(0,20);
-		WebhookService ts = token.getService(WebhookService.class);
 		
 		//TokenManager.instance().storeToken(token);
-		/*Strava strava = new Strava(token);
-		StravaAthlete athlete = strava.getAthlete(36145308);
-		StravaClub club = strava.getClub(275482);
-		ClubService cs = ClubServiceImpl.instance(token);
+		//Strava strava = new Strava(token);
+		//StravaAthlete athlete = strava.getAthlete(36145308);
+		//StravaClub club = strava.getClub(275482);
+		Paging page = new Paging(0,20);
+		//resp.getWriter().write("Total members :: "+club.getMemberCount());
+		/*System.out.println("First Name :: "+athlete.getFirstname());
+		System.out.println("Last Name :: "+athlete.getLastname());
+		System.out.println("email :: "+athlete.getEmail());*/
+		resp.getWriter().write("Hello World! Maven Web Project Example.");
+		LocalDateTime before = LocalDateTime.now();
+		LocalDateTime after = LocalDateTime.of(2019,Month.JANUARY,1,0,0);
+		ActivityService sa = ActivityServiceImpl.instance(token);
+		List<StravaActivity>  ll = sa.listAuthenticatedAthleteActivities(before, after, page);
+		for (StravaActivity stravaActivity : ll) {
+			System.out.println("What is :: "+stravaActivity.getId()+", "+stravaActivity.getName()+",  "+stravaActivity.getWorkoutType().getDescription()+", "+stravaActivity.getElapsedTime()/60 + ", "+stravaActivity.getStartDateLocal()+", "+stravaActivity.getType().getDescription());
+		}
+		/*ClubService cs = ClubServiceImpl.instance(token);
 		List<StravaAthlete> clm = cs.listAllClubAdmins(275482);
 		for (StravaAthlete stravaAthlete : clm) {
 			System.out.println(stravaAthlete.getEmail()+", "+stravaAthlete.getFirstname()+", "+stravaAthlete.getLastname());
@@ -128,20 +107,7 @@ public class SimpleServlet extends HttpServlet {
 			System.out.print(", "+stravaActivity.getDistance());
 			System.out.print(", "+stravaActivity.getTotalElevationGain());
 			System.out.println(", "+(stravaActivity.getType()==null?"":stravaActivity.getType().getDescription()));
-		}
-		resp.getWriter().write("Total members :: "+club.getMemberCount());
-		System.out.println("First Name :: "+athlete.getFirstname());
-		System.out.println("Last Name :: "+athlete.getLastname());
-		System.out.println("email :: "+athlete.getEmail());*/
-		resp.getWriter().write("Hello World! Maven Web Project Example.");
-		LocalDateTime before = LocalDateTime.now();
-		LocalDateTime after = LocalDateTime.of(2019,Month.JANUARY,1,0,0);
-		List<StravaActivity>  ll = sa.listAuthenticatedAthleteActivities(before, after, page);
-		for (StravaActivity stravaActivity : ll) {
-			System.out.println("What is :: "+stravaActivity.getId()+", "+stravaActivity.getName()+",  "+stravaActivity.getWorkoutType().getDescription()+", "+stravaActivity.getElapsedTime()/60 + ", "+stravaActivity.getStartDateLocal()+", "+stravaActivity.getType().getDescription());
-		}
-		
-		AuthorisationService service = new AuthorisationServiceImpl();
+		}*/
 	}
 
 	/**
@@ -149,7 +115,8 @@ public class SimpleServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Do Post method");
+		
+		
 		doGet(request, response);
 	}
 
